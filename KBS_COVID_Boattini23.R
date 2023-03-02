754373cc369fe378db732d699aedbd05518928ec
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2023-03-02 14:04:46 +0000

    Add files via upload

--- KBS_COVID_Boattini23.R
+++ KBS_COVID_Boattini23.R
@@ -152,12 +152,192 @@ Imputed_data %>% group_by(Death_30day) %>% summarise(n=mean(Ferritin, na.rm = TR
 
 fwrite(Imputed_data, "Imputed_data.txt", sep="\t")
 
-# -------------------------------------------------------------------------------------------------
+# --------------------
+# Univarite & Multivariate regression --------------------------------------------------------------
+
+Imputed_data <- fread("Imputed_data.txt", sep="\t", colClasses = "character")
+
+Imputed_data[ , "Age"] <- lapply(Imputed_data[ , "Age"], as.numeric)
+Imputed_data[ , "Charlson_comorbidity_index"] <- lapply(Imputed_data[ , "Charlson_comorbidity_index"], as.numeric)
+Imputed_data[ , "D_dimer"] <- lapply(Imputed_data[ , "D_dimer"], as.numeric)
+Imputed_data[ , "LDH"] <- lapply(Imputed_data[ , "LDH"], as.numeric)
+Imputed_data[ , "CPK"] <- lapply(Imputed_data[ , "CPK"], as.numeric)
+Imputed_data[ , "NT_proBNP"] <- lapply(Imputed_data[ , "NT_proBNP"], as.numeric)
+Imputed_data[ , "Troponin_T"] <- lapply(Imputed_data[ , "Troponin_T"], as.numeric)
+Imputed_data[ , "Ferritin"] <- lapply(Imputed_data[ , "Ferritin"], as.numeric)
+Imputed_data[ , "Creatinine"] <- lapply(Imputed_data[ , "Creatinine"], as.numeric)
+Imputed_data[ , "Lymphocytes_count"] <- lapply(Imputed_data[ , "Lymphocytes_count"], as.numeric)
+Imputed_data[ , "Procalcitonin"] <- lapply(Imputed_data[ , "Procalcitonin"], as.numeric)
+Imputed_data[ , "CRP"] <- lapply(Imputed_data[ , "CRP"], as.numeric)
+Imputed_data[ , "Length_of_stay"] <- lapply(Imputed_data[ , "Length_of_stay"], as.numeric)
+
+Imputed_data <- Imputed_data %>% mutate_if(is.character, as.factor)
+
+# Imputed_data <- Imputed_data %>% mutate_if(is.numeric, scale)
 
 
+# --------------------------------------
+# Rectal Carriers  Table 1 & Table 2  --------------------------------------------------------------
+
+names(Imputed_data)
+
+storage_rectal_carriers <- data.frame()
+
+for(i in names(Imputed_data)[-36]){
+  print(i)
+  cat(" - - - - - - - - - - - - - - - - - - - - - - - - -")
+  model <- glm(Rectal_carriage ~ get(i), data = Imputed_data, family=binomial())
+  storage_rectal_carriers <- rbind(storage_rectal_carriers, tidy(model)[2,])
+}
 
 
+storage_rectal_carriers <- data.frame(
+  storage_rectal_carriers %>% 
+             bind_cols(
+               data.frame(names(Imputed_data)[-36])
+               )
+  )  
+
+storage_rectal_carriers <- storage_rectal_carriers[,-1]
+
+names(storage_rectal_carriers)[5] <- "feature"
+
+storage_rectal_carriers <- storage_rectal_carriers[,c(5,3,4,1,2)]
+
+storage_rectal_carriers$OR <- exp(storage_rectal_carriers$estimate)
+storage_rectal_carriers$Upper <- exp(storage_rectal_carriers$estimate + storage_rectal_carriers$std.error)
+storage_rectal_carriers$Lower <- exp(storage_rectal_carriers$estimate - storage_rectal_carriers$std.error)
+
+storage_rectal_carriers$OR <- exp(storage_rectal_carriers$estimate)
+
+storage_rectal_carriers %>% filter(p.value<0.05)
+
+model <- (glm(Rectal_carriage ~ 
+      ICU+
+      Neo+Diabetes+
+      Pulmonary_embolism+
+      IMV_ecmo+
+      Hydroxychloroquine+
+      Lopinavir_ritonavir+
+      Infection+
+      KPC_EB_or_ACB_respiratory_tract_infection  , data = Imputed_data, family=binomial()))
+
+model <- tidy(model)
+
+model$OR <- exp(model$estimate)
+model$Upper <- exp(model$estimate + model$std.error)
+model$Lower <- exp(model$estimate - model$std.error)
+
+model <- model[,c(1,4,5,2,3,6,7,8)]
+
+# ---------------------------------------------------------------------------------------------
+# Infection Table 3 & Table 4  --------------------------------------------------------------
 
 
+names(Imputed_data)
 
-summary(glm( Community_adquired_SARS_CoV_2_infection1  ~ ., data = Imputed_data, family = binomial))
+storage_infection <- data.frame()
+
+for(i in names(Imputed_data)[-37]){
+  print(i)
+  cat(" - - - - - - - - - - - - - - - - - - - - - - - - -")
+  model <- glm(Infection ~ get(i), data = Imputed_data, family=binomial())
+  storage_infection <- rbind(storage_infection, tidy(model)[2,])
+}
+
+
+storage_infection <- data.frame(
+  storage_infection %>% 
+             bind_cols(
+               data.frame(names(Imputed_data)[-37])
+               )
+  )  
+
+storage_infection <- storage_infection[,-1]
+
+names(storage_infection)[5] <- "feature"
+
+storage_infection <- storage_infection[,c(5,3,4,1,2)]
+
+storage_infection$OR <- exp(storage_infection$estimate)
+storage_infection$Upper <- exp(storage_infection$estimate + storage_infection$std.error)
+storage_infection$Lower <- exp(storage_infection$estimate - storage_infection$std.error)
+
+
+storage_infection %>% filter(p.value<0.05)
+
+model <- (glm(Infection ~ 
+      Pulmonary_embolism+
+        CPK+
+      Ferritin+
+      Lymphocytes_count+
+      CRP+
+      IMV_ecmo , data = Imputed_data, family=binomial()))
+
+model <- tidy(model)
+
+model$OR <- exp(model$estimate)
+model$Upper <- exp(model$estimate + model$std.error)
+model$Lower <- exp(model$estimate - model$std.error)
+
+model <- model[,c(1,4,5,2,3,6,7,8)]
+data.frame(model)
+
+# --------------------------------------------------------------------------
+# 30 day mortality Table 5 & Table 6  --------------------------------------------------------------
+
+
+names(Imputed_data)
+
+storage_30daymortality <- data.frame()
+
+for(i in names(Imputed_data)[-43]){
+  print(i)
+  cat(" - - - - - - - - - - - - - - - - - - - - - - - - -")
+  model <- glm(Death_30day ~ get(i), data = Imputed_data, family=binomial())
+  storage_30daymortality <- rbind(storage_30daymortality, tidy(model)[2,])
+}
+
+
+storage_30daymortality <- data.frame(
+  storage_30daymortality %>% 
+             bind_cols(
+               data.frame(names(Imputed_data)[-43])
+               )
+  )  
+
+storage_30daymortality <- storage_30daymortality[,-1]
+
+names(storage_30daymortality)[5] <- "feature"
+
+storage_30daymortality <- storage_30daymortality[,c(5,3,4,1,2)]
+
+storage_30daymortality$OR <- exp(storage_30daymortality$estimate)
+storage_30daymortality$Upper <- exp(storage_30daymortality$estimate + storage_30daymortality$std.error)
+storage_30daymortality$Lower <- exp(storage_30daymortality$estimate - storage_30daymortality$std.error)
+
+storage_30daymortality$Lower <- round(storage_30daymortality$Lower,digits=5)
+
+storage_infection %>% filter(p.value<0.05)
+
+model <- (glm(Death_30day ~ 
+      ICU  +
+      Pulmonary_embolism  +
+      CPK  +
+      Ferritin  +
+      Lymphocytes_count +
+      CRP  +
+       IMV_ecmo  +
+       Rectal_carriage   , data = Imputed_data, family=binomial()))
+
+model <- tidy(model)
+
+model$OR <- exp(model$estimate)
+model$Upper <- exp(model$estimate + model$std.error)
+model$Lower <- exp(model$estimate - model$std.error)
+
+model <- model[,c(1,4,5,2,3,6,7,8)]
+data.frame(model)
+
+
+# ----------------------------
